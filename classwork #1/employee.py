609df07a6b61d60303cc720516aabdde7914248c
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-03-02 19:52:29 +0400

    code simplification

--- classwork #1/employee.py
+++ classwork #1/employee.py
@@ -7,13 +7,16 @@ class Employee:
         self.age = age
         self.salary = salary
 
-    def print_info(self): print(f"Employee name: {self.name} {self.surname}\nSalary: {self.salary}\nAge: {self.age}")
-
-employee_list = []
+    def print_info(self):
+        print(f"Employee name: {self.name} {self.surname}\nSalary: {self.salary}\nAge: {self.age}")
 
 with open("dataset1.csv", 'r') as csv_file:
-    csv_reader = csv.reader(csv_file); next(csv_reader)
-    for line in csv_reader: employee_list.append(Employee(line[0], line[1], line[2], line[3]))
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    employee_list = [Employee(line[0], line[1], line[2], line[3]) for line in csv_reader]
 
-print("Employee with the least salary:"); min(employee_list, key=lambda emp: emp.salary).print_info(); print()
-print("The oldest employee:"); max(employee_list, key=lambda emp: emp.age).print_info()
+print("Employee with the least salary:")
+min(employee_list, key=lambda emp: emp.salary).print_info()
+print()
+print("The oldest employee:")
+max(employee_list, key=lambda emp: emp.age).print_info()
