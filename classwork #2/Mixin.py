ba3b0408d26f3991eee3f23827cf558c8c376706
hexley21 <A.kupreishvili.21@gmail.com>
2022-03-16 22:31:50 +0400

    Mixin class - corrected

--- classwork #2/Mixin.py
+++ classwork #2/Mixin.py
@@ -1,5 +1,9 @@
-class Person():
-    def __init__(self, fname, lname, phone) -> None:
+class CallMixin():
+    def call(self):
+        print(f"Pending a call to  +{self.phone}")
+        
+class Person(CallMixin):
+    def __init__(self, fname, lname, phone):
         self.fname = fname
         self.lname = lname
         self.phone = phone
@@ -7,11 +11,12 @@ class Person():
     def info(self):
         print(f"First name: {self.fname}\nLast name: {self.lname}")
 
-class CallMixin(Person):
     def call(self):
         print(f"Pending a call to {self.fname} {self.lname} on a number +{self.phone}")
 
 
-fbi_agent = CallMixin("Konstantine", "Mgeladze", 995541034141)
+
+
+fbi_agent = Person("Konstantine", "Mgeladze", 995541034141)
 fbi_agent.info()
 fbi_agent.call()
