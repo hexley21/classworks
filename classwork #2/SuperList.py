6317bad35e25e9153792cfe006d7d288d6e726cd
hexley21 <A.kupreishvili.21@gmail.com>
2022-03-16 22:32:34 +0400

    Code - simplified

--- classwork #2/SuperList.py
+++ classwork #2/SuperList.py
@@ -1,21 +1,13 @@
-import random
-
-
 class SuperList(list):    
     def min(self):
         return min(self)
     def max(self):
         return max(self)
 
-super_list = SuperList()
-super_list.extend([1, 2, 3, 4, 5, 6, 7])
+super_list = SuperList([1, 2, 3, 4, 5, 6, 7])
+wow_list = SuperList([-7, -6, -5, -4, -3, -2, -1, 0])
+cool_list = SuperList([-23, 29, 46, -87, 65])
 
-wow_list = SuperList()
-wow_list.extend([-7, -6, -5, -4, -3, -2, -1, 0])
-
-
-cool_list = SuperList()
-cool_list.extend([-23, 29, 46, -87, 65])
 
 def test_class(super_list: SuperList):
     print(super_list.min())
@@ -25,4 +17,3 @@ def test_class(super_list: SuperList):
 test_class(super_list)
 test_class(wow_list)
 test_class(cool_list)
-
